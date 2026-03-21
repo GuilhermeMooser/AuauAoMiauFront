e9d8ef69758cfa8ac8b26e437476ecedb6271204
Guilherme Moser <guilhermemoserm@gmail.com>
2026-03-20 22:01:35 -0300

    fix: Added redict to login when user is not  authenticated

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,21 @@
-import { getAuth } from "@/utils/auth";
-import axios, { isAxiosError } from "axios";
+import {getAuth} from "@/utils/auth";
+import axios, {isAxiosError} from "axios";
 
 export const api = axios.create({
   baseURL: "http://localhost:3000/api",
   withCredentials: true,
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("authToken");
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  },
+);
 
 //  api.interceptors.request.use(
 //   (config) => {
@@ -33,14 +44,4 @@ export const api = axios.create({
 //   }
 // );
 
-// // Response interceptor for error handling
-// api.interceptors.response.use(
-//   (response) => response,
-//   (error) => {
-//     if (error.response?.status === 401) {
-//       localStorage.removeItem('authToken');
-//       window.location.href = '/login';
-//     }
-//     return Promise.reject(error);
-//   }
-// );
+// Response interceptor for error handling
