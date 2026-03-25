a606ae077b5573eac0817498371534924e4a478a
Guilherme Moser <guilhermemoserm@gmail.com>
2026-03-25 20:50:27 -0300

    fix: Fixed environments variables

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import {getAuth} from "@/utils/auth";
 import axios, {isAxiosError} from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_APP_API_URL,
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
