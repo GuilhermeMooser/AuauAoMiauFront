1326633e7e826d2f894246caef2bd4124bced6a8
Guilherme Moser <guilhermemoserm@gmail.com>
2026-03-25 22:31:13 -0300

    feat: new test with domain console.log

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -3,6 +3,7 @@ import { parseCookies, destroyCookie, setCookie } from "nookies";
 import { Login } from "@/types/login";
 
 export const authenticate = (login: Login) => {
+  console.log('domain:', getViteApiDomain());
   setCookie(null, "login", JSON.stringify(login), {
     maxAge: 1 * 60 * 60,
     path: "/",
